cf320f8194c71e3b57288160fd61c2cf59cf2361
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-06 09:52:08 +0600

    root route added

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,10 @@ const app = express();
 
 
 app.get("/", (req :Request , res: Response)=>{
-    
+    res.status(200).json({
+        message: "This is the root route",
+        path: req.path,
+    })
 })
 
 
