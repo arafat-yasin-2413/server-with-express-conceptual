b6b44d08cb483579cac015d541d52168ea1361f2
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-06 11:04:59 +0600

    post api created

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,46 @@ import {Pool} from "pg";
 const app = express();
 app.use(express.json());
 
-const pool = new Pool
+const pool = new Pool({
+    connectionString: 'postgresql://neondb_owner:npg_o0XgA6pjPULG@ep-red-river-a8cato4q-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require'
+})
+
+
+const initDB = async()=>{
+    await pool.query(
+        `   
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(250) NOT NULL,
+            email VARCHAR(150) UNIQUE,
+            password TEXT NOT NULL,
+            age INT,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+
+        `)
+
+        console.log("Database connected");
+}
+
+initDB();
 
 app.post("/users", async(req: Request, res: Response)=>{
-    const body = req.body;
-    console.log('printing req.body--------');
-    console.log(body);
-})
+    const {name , email, password } = req.body;
+
+    const result = await pool.query (`
+            INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *
+        `,[name, email, password]);
+
+        // console.log(result);
+
+        res.status(201).json({
+            success: true,
+            message: "User Created",
+            data: result.rows[0]
+        })
+});
 
 
 app.get("/", (req :Request , res: Response)=>{
