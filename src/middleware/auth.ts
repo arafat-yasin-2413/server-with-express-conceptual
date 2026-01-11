5ef270ab767fff6f9c94785ee7892b3c8387871a
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-11 12:23:49 +0600

    types added

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,10 +1,28 @@
-import { NextFunction, Request, Response } from "express"
-
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { secret } from "../modules/auth/auth.service";
+import { pool } from "../database/db";
 
 const auth = () =>{
     return async (req:Request, res:Response, next:NextFunction) =>{
-        const tokenF = req.headers.authorization;
-        console.log('printing in auth.ts ----',tokenF);
+        const token = req.headers.authorization;
+        // console.log('printing in auth.ts ----',token);
+
+        if(!token) {
+            throw new Error("You are not authorized")
+        }
+
+        const decoded = jwt.verify(token, secret) as JwtPayload;
+        
+        const user = await pool.query(`SELECT * FROM users WHERE email=$1`,[decoded.email]) 
+        
+        if(user.rows.length === 0) {
+            throw new Error("User not found");
+        }
+
+        console.log('Decoded token : --- : ', decoded);
+        req.user = decoded;
+
         next();
     }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,35 +2,39 @@ import bcrypt from "bcryptjs";
 import { pool } from "../../database/db";
 import jwt from "jsonwebtoken";
 
+export const secret = "KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
+
 const loginUser = async (email: string, password: string) => {
-	const result = await pool.query(`SELECT * FROM users WHERE email=$1`, [email]);
-    const user = result.rows[0];
-    // console.log('auth service User : ---', result);
-    // console.log('--------------------', result.rows[0]);
+  const result = await pool.query(`SELECT * FROM users WHERE email=$1`, [
+    email,
+  ]);
+  const user = result.rows[0];
+  // console.log('auth service User : ---', result);
+  // console.log('--------------------', result.rows[0]);
 
+  if (result.rowCount === 0) {
+    throw new Error("User not found!");
+  }
 
-	if(result.rowCount === 0) {
-		throw new Error("User not found!");
-	}
+  const matchedPassword = await bcrypt.compare(
+    password,
+    result.rows[0].password
+  );
 
-	const matchedPassword = await bcrypt.compare(password, result.rows[0].password);
+  if (!matchedPassword) {
+    throw new Error("Invalid credentials");
+  }
 
-	if (!matchedPassword) {
-		throw new Error("Invalid credentials");
-	}
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  };
+  const token = jwt.sign(jwtPayload, secret, { expiresIn: "7d" });
 
-	const jwtPayload = {
-		id:user.id,
-		name: user.name,
-		email: user.email
-	}
-	const secret ="KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
-	const token = jwt.sign (jwtPayload,secret,{expiresIn: "7d"});
-
-
-	return {user, token}; 
+  return { user, token };
 };
 
 export const authService = {
-	loginUser,
+  loginUser,
 };
