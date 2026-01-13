3f68db30e923a0f60f1f59a7239f46836cc9ad91
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-13 09:55:01 +0600

    prettier re-formatted

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,34 +3,35 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { secret } from "../modules/auth/auth.service";
 import { pool } from "../database/db";
 
-const auth = (...roles : ('admin' | 'user')[]) =>{
-    
-    console.log('Role from user route parameter : ', roles);
+const auth = (...roles: ("admin" | "user")[]) => {
+    console.log("Role from user route parameter : ", roles);
 
-    return async (req:Request, res:Response, next:NextFunction) =>{
+    return async (req: Request, res: Response, next: NextFunction) => {
         const token = req.headers.authorization;
         // console.log('printing in auth.ts ----',token);
 
-        if(!token) {
-            throw new Error("You are not authorized")
+        if (!token) {
+            throw new Error("You are not authorized");
         }
 
         const decoded = jwt.verify(token, secret) as JwtPayload;
-        
-        const user = await pool.query(`SELECT * FROM users WHERE email=$1`,[decoded.email]) 
-        
-        if(user.rows.length === 0) {
+
+        const user = await pool.query(`SELECT * FROM users WHERE email=$1`, [
+            decoded.email,
+        ]);
+
+        if (user.rows.length === 0) {
             throw new Error("User not found");
         }
 
-        console.log('Decoded token : --- : ', decoded);
+        console.log("Decoded token : --- : ", decoded);
         req.user = decoded;
 
-        if(roles.length && !roles.includes(decoded.role)){
+        if (roles.length && !roles.includes(decoded.role)) {
             throw new Error("You are not authorized to watch all users!");
         }
         next();
-    }
-}
+    };
+};
 
 export default auth;
